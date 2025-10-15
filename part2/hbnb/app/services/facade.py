3f84a29e701379858cd6813a90998976fcf7f6a2
N-Haitu31 <nguyenhaitu31@gmail.com>
2025-10-15 10:01:38 +0200

    feat: Implement CRUD endpoints for Place entity

--- part2/hbnb/app/services/facade.py
+++ part2/hbnb/app/services/facade.py
@@ -1,4 +1,5 @@
 from app.persistence.repository import InMemoryRepository
+from app.models.place import Place
 
 class HBnBFacade:
     def __init__(self):
@@ -12,7 +13,24 @@ class HBnBFacade:
         # Logic will be implemented in later tasks
         pass
 
-    # Placeholder method for fetching a place by ID
+    # --------PLACE METHODS--------
+    def create_place(self, place_data):
+        place = Place(**place_data)
+        self.place_repo.add(place)
+        return place
+
     def get_place(self, place_id):
-        # Logic will be implemented in later tasks
-        pass
+        return self.place_repo.get(place_id)
+
+    def get_all_places(self):
+        return self.place_repo.get_all()
+
+    def update_place(self, place_id, place_data):
+        place = self.place_repo.get(place_id)
+        if not place:
+            return None
+        for key, value in place_data.items():
+            if key is not "amenities" and key is not "reviews":
+                setattr(place, key, value)
+        self.place_repo.update(place_id, place_data)
+        return place
