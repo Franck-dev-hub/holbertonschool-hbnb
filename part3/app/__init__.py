fb6c32ee67386409a293cd9f0c920577e58a8cee
Franck <franck.spadotto@yahoo.com>
2025-10-27 10:28:40 +0100

    Add required modules + include config object

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,5 +1,14 @@
 from flask import Flask
 from flask_restx import Api
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+
+bcrypt = Bcrypt()
+jwt = JWTManager()
+db = SQLAlchemy()
+
 from app.api.v1.places import api as places_ns
 from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
@@ -8,6 +17,7 @@ from app.api.v1.reviews import api as reviews_ns
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(config_class)
     api = Api(
         app, version='1.0',
         title='HBnB API',
@@ -15,13 +25,21 @@ def create_app():
         doc='/api/v1/'
     )
 
-    # Register the users namespace
+    CORS(app, ressources={
+        r"/*": {
+            "origins": ["http://localhost:5000", "http://127.0.0.1:5000"],
+            "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+            "allow_headers": ["Content-Type", "Authorization"]
+        }
+    })
+
     api.add_namespace(users_ns, path='/api/v1/users')
-    # Register the places namespaces
     api.add_namespace(places_ns, path='/api/v1/places')
-    # Register the amenities namespace
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
-    # Register the reviews namespace
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
+
+    db.init_app(app)
+    bcrypt.init_app(app)
+    jwt.init_app(app)
 
     return app
