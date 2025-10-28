9219a71846682d6f2419c8112df46f00fd9374b7
AdelMej <adelmejrissi@gmail.com>
2025-10-28 08:57:08 +0100

    feat: added config for jwt-auth added flask-jwt-extended in requirement.txt added a auth endpoint with login and a protected endpoint for testing

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,12 +1,21 @@
 from flask import Flask
 from flask_restx import Api
+from flask_jwt_extended import JWTManager
+from config import config
+
 from app.api.v1.places import api as places_ns
 from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.reviews import api as reviews_ns
+from app.api.v1.auth import api as auth_ns
+from app.api.v1.protected import api as protected_ns
+
+# global jwt manager
+jwt = JWTManager()
 
 
 def create_app():
+    # initializing app
     app = Flask(__name__)
     api = Api(
         app, version='1.0',
@@ -14,6 +23,12 @@ def create_app():
         description='HBnB Application API',
         doc='/api/v1/'
     )
+
+    # initiaalize the JWTManager
+    jwt.init_app(app)
+
+    # load config
+    app.config.from_object(config["development"])
 
     # Register the users namespace
     api.add_namespace(users_ns, path='/api/v1/users')
@@ -23,5 +38,9 @@ def create_app():
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
     # Register the reviews namespace
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
+    # Register the auth namespace
+    api.add_namespace(auth_ns, path="/api/v1/auth")
+    # import protected route
+    api.add_namespace(protected_ns, path="/api/v1/protected")
 
     return app
--- part3/config.py
+++ part3/config.py
@@ -1,11 +1,15 @@
 import os
 
+
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
     DEBUG = False
 
+
 class DevelopmentConfig(Config):
     DEBUG = True
+    SECRET_KEY = "dev_secret_key123"
+
 
 config = {
     'development': DevelopmentConfig,
