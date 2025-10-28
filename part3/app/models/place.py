74c8998e3fd0167e9a28e15d9e22d9f95cb7a4de
N-Haitu31 <nguyenhaitu31@gmail.com>
2025-10-28 15:28:40 +0100

    feat: Add places mapping

--- part3/app/models/place.py
+++ part3/app/models/place.py
@@ -1,7 +1,20 @@
 from app.models.base import BaseModel
+from app import db
 
 
 class Place(BaseModel):
+    __tablename__ = 'places'
+
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    latitude = db.Column(db.Float, nullable=False)
+    longitude = db.Column(db.Float, nullable=False)
+    rooms = db.Column(db.Integer, nullable=False)
+    capacity = db.Column(db.Integer, nullable=False)
+    surface = db.Column(db.Float, nullable=False)
+    owner_id = db.Column(db.String(36), db.ForeignKey('users.id', ondelete='CASCADE'), nullable=False)
+
     def __init__(self, title, price, latitude, longitude, owner_id, rooms, description=None, capacity=0, surface=0, amenities=[], reviews=[]):
         super().__init__()
         if title is not None and 0 < len(title) <= 100:
