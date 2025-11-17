2dee3b81622c9e865377ac38d66af0dddc75ce04
Franck <franck.spadotto@yahoo.com>
2025-11-17 16:57:13 +0100

    dynamicly display header and footer

---
+++ part4/header-footer.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+  fetch("header.html")
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById("header").innerHTML = data;
+    })
+    .catch(err => console.error("Error loading header :", err));
+
+  fetch("footer.html")
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById("footer").innerHTML = data;
+    })
+    .catch(err => console.error("Error loading footer :", err));
+
+});
