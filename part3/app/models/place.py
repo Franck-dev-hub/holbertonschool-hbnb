fc94285828a21e22569d7112c79d56e6d9c42bab
Franck <franck.spadotto@yahoo.com>
2025-11-19 21:02:55 +0100

    add missing infos + strengthens logic

--- part3/app/models/place.py
+++ part3/app/models/place.py
@@ -7,7 +7,6 @@ place_amenity = db.Table('place_amenity',
     db.Column('amenity_id', db.String, db.ForeignKey('amenities.id'), primary_key=True)
 )
 
-
 class Place(BaseModel):
     __tablename__ = 'places'
 
@@ -16,92 +15,39 @@ class Place(BaseModel):
     price = db.Column(db.Float, nullable=False)
     latitude = db.Column(db.Float, nullable=False)
     longitude = db.Column(db.Float, nullable=False)
-
     owner_id = db.Column(db.String, db.ForeignKey("users.id"), nullable=False)
+    rooms = db.Column(db.Integer, nullable=False)
+    capacity = db.Column(db.Integer, nullable=False)
+    surface = db.Column(db.Float, nullable=False)
     amenities = db.relationship('Amenity', secondary=place_amenity, lazy='subquery',
-                              backref=db.backref('place', lazy=True))
+                                backref=db.backref('place', lazy=True))
     reviews = db.relationship('Review', backref='place', lazy=True)
 
-    def __init__(self, title, price, latitude, longitude, owner_id, rooms, description=None, capacity=0, surface=0, amenities=[], reviews=[]):
+    def __init__(self, title, price, latitude, longitude, owner_id, rooms, description=None, capacity=0, surface=0):
         super().__init__()
-        if title is not None and 0 < len(title) <= 100:
-            self.__title = title
-        else:
+        if not title or len(title) > 100:
             raise ValueError
+        if price is None or price < 0:
+            raise ValueError
+        if latitude is None or not -90 <= latitude <= 90:
+            raise ValueError
+        if longitude is None or not -180 <= longitude <= 180:
+            raise ValueError
+
+        self.title = title
+        self.price = price
+        self.latitude = latitude
+        self.longitude = longitude
         self.description = description
-        if price is not None and price >= 0:
-            self.__price = price
-        else:
-            raise ValueError
-        if latitude is not None and -90.0 <= latitude <= 90.0:
-            self.__latitude = latitude
-        else:
-            raise ValueError
-        if longitude is not None and -180.0 <= longitude <= 180.0:
-            self.__longitude = longitude
-        else:
-            raise ValueError
-        if rooms <= 0:
-            raise ValueError
-        if surface < 0:
-            raise ValueError
-        if capacity <= 0:
-            raise ValueError
         self.owner_id = owner_id
         self.rooms = rooms
         self.capacity = capacity
         self.surface = surface
-        self.reviews = []
         self.amenities = []
+        self.reviews = []
 
     def add_review(self, review):
-        """Add a review to the place."""
         self.reviews.append(review)
 
     def add_amenity(self, amenity):
-        """Add an amenity to the place."""
         self.amenities.append(amenity)
-
-    @property
-    def title(self):
-        return self.__title
-
-    @title.setter
-    def title(self, title):
-        if title is not None and 0 < len(title) <= 100:
-            self.__title = title
-        else:
-            raise ValueError
-
-    @property
-    def price(self):
-        return self.__price
-
-    @price.setter
-    def price(self, price):
-        if price is not None and price >= 0:
-            self.__price = price
-        else:
-            raise ValueError
-
-    @property
-    def latitude(self):
-        return self.__latitude
-
-    @latitude.setter
-    def latitude(self, latitude):
-        if latitude is not None and -90.0 <= latitude <= 90.0:
-            self.__latitude = latitude
-        else:
-            raise ValueError
-
-    @property
-    def longitude(self):
-        return self.__longitude
-
-    @longitude.setter
-    def longitude(self, longitude):
-        if longitude is not None and -180.0 <= longitude <= 180.0:
-            self.__longitude = longitude
-        else:
-            raise ValueError
