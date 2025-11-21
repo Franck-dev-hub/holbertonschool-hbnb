6995bed36ac2d7bbcdddd91b1f167e51ebd20e4f
Franck <franck.spadotto@yahoo.com>
2025-11-21 09:43:54 +0100

    add auth management

--- part4/js/cookies.js
+++ part4/js/cookies.js
@@ -14,3 +14,28 @@ function getCookie(name) {
 function deleteCookie(name) {
   document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
 }
+
+// Check if user is connected
+function isAuthenticated() {
+    return getCookie("access_token") !== null;
+}
+
+// Send to login.html if not logged
+function requireAuthentication() {
+    const token = getCookie("access_token");
+    if (!token) {
+        window.location.href = "login.html";
+        return null;
+    }
+    return token;
+}
+
+// Add token to auth
+function getAuthHeaders() {
+    const headers = { "Content-Type": "application/json" };
+    const token = getCookie("access_token");
+    if (token) {
+        headers["Authorization"] = `Bearer ${token}`;
+    }
+    return headers;
+}
